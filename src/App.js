8ba28f42cb7aedfd8509863a9e378f29e6935727
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-11-29 13:07:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import Container from './Components/Container/Container';
 
 export default function App() {
-  return (
-    <Container>
-      {/* <Box type="small" classNames="big red" styles={{ color: '#fff' }} />
-      <Box type="medium" />
-      <Box type="large" /> */}
-    </Container>
-  );
+  return <Container></Container>;
 }
